7d4fc00b0ba06d668e905b805c3d4561c57b79d3
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2021-04-22 11:36:39 +0530

    Implemented location feature

--- src/Components/Detectlocation.js
+++ src/Components/Detectlocation.js
@@ -1,62 +1,75 @@
 import { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import MyLocationIcon from "@material-ui/icons/MyLocation";
-import { green, yellow } from "@material-ui/core/colors";
 const Detectlocation = () => {
-  const [values, setValues] = useState({
-    disabled: false,
-    color: green["A400"],
-    lat: 0,
-    lon: 0,
-  });
-  const handleChange = ({ name, value }) =>
-    setValues({ ...values, [name]: value });
-  const success = (pos) => {
-    var crd = pos.coords;
-
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-    handleChange('lat',crd.latitude);
-    handleChange('lon',crd.longitude);
-  }
-
-  const errors = (err) => {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
+  const location = [];
+  const [disabled, setdisabled] = useState(false);
+  const [detectlocation, setDetectlocation] = useState(false);
+  //const [, setUpdate] = useState();
   useEffect(() => {
     let options = {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0,
     };
-    if (navigator.geolocation) {
-      navigator.permissions.query({ name: "geolocation" }).then((result) => {
-        if (result.state === "granted") {
-          console.log("result state = ", result.state);
-          navigator.geolocation.getCurrentPosition(success);
-        } else if (result.state === "prompt") {
-          console.log("result state = ", result.state);
-          navigator.geolocation.getCurrentPosition(success, errors, options);
-        } else if (result.state === "denied") {
-          console.log("result state = ", result.state);
-        }
-        result.onchange = function () {
-          console.log(result.state);
-        };
-      });
-    } else {
-      alert("Sorry Not available!");
-    }
-  },[]);
 
+    const success = (pos) => {
+      let crd = pos.coords;
+
+      // console.log("Your current position is:");
+      // console.log(`Latitude : ${crd.latitude}`);
+      // console.log(`Longitude: ${crd.longitude}`);
+      // console.log(`More or less ${crd.accuracy} meters.`);
+      location.push({ latitude: crd.latitude, longitude: crd.longitude });
+    };
+
+    const errors = (err) => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    for (let i = 0; i < 1; i++) {
+      if (detectlocation === true) {
+        if (navigator.geolocation) {
+          navigator.permissions
+            .query({ name: "geolocation" })
+            .then((result) => {
+              if (result.state === "granted") {
+                console.log("result state = ", result.state);
+                navigator.geolocation.getCurrentPosition(success);
+              } else if (result.state === "prompt") {
+                console.log("result state = ", result.state);
+                navigator.geolocation.getCurrentPosition(
+                  success,
+                  errors,
+                  options
+                );
+              } else if (result.state === "denied") {
+                alert(
+                  "Location Permission Denied! Emable permission to detect location"
+                );
+                setdisabled(true);
+              }
+              result.onchange = function () {
+                console.log(result.state);
+              };
+            });
+        } else {
+          alert("Sorry Not available!");
+        }
+      }
+    }
+  });
+
+  useEffect(() => {
+    console.log("Emit location", location);
+  });
   return (
     <Button
       variant="contained"
-      disabled={values.disabled}
-      style={{ backgroundColor: values.color }}
+      disabled={disabled}
+      color="primary"
       startIcon={<MyLocationIcon />}
+      onClick={() => setDetectlocation(true)}
     >
       Detect Location
     </Button>
